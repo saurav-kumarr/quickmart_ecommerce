6c01e18d9e36ed44030bde0a242ae65013a07b83
Saurav Kumar <csaurav2014@gmail.com>
2025-07-20 22:00:23 +0530

    first commit of project Quickmart

---
+++ src/main/java/com/quickmart/QuickmartApplication.java
@@ -0,0 +1,14 @@
+package com.quickmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickmartApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(QuickmartApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/quickmart/controller/CategoryController.java
@@ -0,0 +1,20 @@
+package com.quickmart.controller;
+
+import com.quickmart.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    private List<Category> categories = new ArrayList<>();
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+
+}
