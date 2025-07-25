f4059a574209215ab3cba154408e1fc7d26d5723
Saurav Kumar <csaurav2014@gmail.com>
2025-07-25 21:21:50 +0530

    Add Validation,Lombok and Valid in Controller

--- src/main/java/com/quickmart/controller/CategoryController.java
+++ src/main/java/com/quickmart/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.quickmart.controller;
 
 import com.quickmart.model.Category;
 import com.quickmart.service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,7 +28,7 @@ public class CategoryController {
     }
 
     @PostMapping("/public/categories")
-    public ResponseEntity<String> createCategory(@RequestBody Category category) {
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category) {
             categoryService.createCategory(category);
 
         return new ResponseEntity<>("Category created successfully",HttpStatus.CREATED);
