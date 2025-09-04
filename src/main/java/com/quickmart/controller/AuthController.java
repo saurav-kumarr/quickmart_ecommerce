10304fc800a292f29641a3ebb4bc534501d61bb6
Saurav Kumar <csaurav2014@gmail.com>
2025-09-05 01:07:33 +0530

    Change Token based authorization to Cookie based for convience. For Token Solution go to previous commit. (COOKIE)

--- src/main/java/com/quickmart/controller/AuthController.java
+++ src/main/java/com/quickmart/controller/AuthController.java
@@ -13,7 +13,9 @@ import com.quickmart.security.response.UserInfoResponse;
 import com.quickmart.security.services.UserDetailsImpl;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.security.authentication.AuthenticationManager;
@@ -77,15 +79,17 @@ public class AuthController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
 
-        String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
+        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
 
         List<String> roles = userDetails.getAuthorities().stream()
                 .map(item -> item.getAuthority())
                 .collect(Collectors.toList());
 
         UserInfoResponse response = new UserInfoResponse(userDetails.getId(),userDetails.getUsername(),
-                roles,jwtToken);
-        return ResponseEntity.ok(response);
+                roles);
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE,
+                jwtCookie.toString())
+                .body(response);
     }
 
     @PostMapping("/signup")
--- src/main/java/com/quickmart/security/jwt/JwtUtils.java
+++ src/main/java/com/quickmart/security/jwt/JwtUtils.java
@@ -1,17 +1,22 @@
 package com.quickmart.security.jwt;
 
+import com.quickmart.security.services.UserDetailsImpl;
+import com.quickmart.security.services.UserDetailsServiceImpl;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.WebUtils;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
@@ -27,20 +32,44 @@ public class JwtUtils {
     @Value("${spring.app.jwtExpirationMs}")
     private int jwtExpirationMs;
 
+    @Value("${spring.ecom.app.jwtCookieName}")
+    private String jwtCookie;
+
 
      //Getting JWT From Header
-    public String getJwtFromHeader(HttpServletRequest request){
+   /* public String getJwtFromHeader(HttpServletRequest request){
         String bearerToken = request.getHeader("Authorization");
         logger.debug("Authorization Header:{}",bearerToken);
         if(bearerToken != null && bearerToken.startsWith("Bearer ")){
             return bearerToken.substring(7); // Remove Bearer Prefix
         }
         return null;
+    }*/
+
+    public String getJwtFromCookies(HttpServletRequest request){
+
+        Cookie cookie = WebUtils.getCookie(request, jwtCookie);
+        if(cookie != null){
+            System.out.println("COOKIE: " + cookie.getValue());
+            return cookie.getValue();
+        } else {
+            return null;
+        }
+
+    }
+
+    public ResponseCookie generateJwtCookie(UserDetailsImpl userPrincipal){
+        String jwt = generateTokenFromUsername(userPrincipal.getUsername());
+        ResponseCookie cookie = ResponseCookie.from(jwtCookie,jwt)
+                .path("/api")
+                .maxAge(24*60*60)
+                .httpOnly(false)
+                .build();
+        return cookie;
     }
 
     //Generating Token From Username
-    public String generateTokenFromUsername(UserDetails userDetails){
-        String username = userDetails.getUsername();
+    public String generateTokenFromUsername(String username){
         return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date())
