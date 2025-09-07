5ff81288ba8378d6a90afee890281bb20e8f6f02
Saurav Kumar <csaurav2014@gmail.com>
2025-09-07 20:19:21 +0530

    Implement feature to fetch all carts.

--- src/main/java/com/quickmart/service/CartService.java
+++ src/main/java/com/quickmart/service/CartService.java
@@ -2,7 +2,13 @@ package com.quickmart.service;
 
 import com.quickmart.payload.CartDTO;
 
+import java.util.List;
+
 public interface CartService {
+
+
     CartDTO addProductToCart(Long productId, Integer quantity);
+
+    List<CartDTO> getAllCarts();
 
 }
