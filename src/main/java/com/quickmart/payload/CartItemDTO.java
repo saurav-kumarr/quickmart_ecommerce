9d06eaf479497d2a109bebda6c7ab968af05b067
Saurav Kumar <csaurav2014@gmail.com>
2025-12-30 02:41:46 +0530

    Refactor CartItemDTO

--- src/main/java/com/quickmart/payload/CartItemDTO.java
+++ src/main/java/com/quickmart/payload/CartItemDTO.java
@@ -10,11 +10,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CartItemDTO {
 
-    private Long cartItemId;
-
-    private CartDTO cart;
-    private ProductDTO productDTO;
+    private Long productId;
     private Integer quantity;
-    private Double discount;
-    private Double productPrice;
+
+//    private Long cartItemId;
+//
+//    private CartDTO cart;
+//    private ProductDTO productDTO;
+//    private Integer quantity;
+//    private Double discount;
+//    private Double productPrice;
 }
