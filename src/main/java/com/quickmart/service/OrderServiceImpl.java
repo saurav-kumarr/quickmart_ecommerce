cdc750ca4bd2f5387d5ec1021cc2142a5796bbf7
Saurav Kumar <csaurav2014@gmail.com>
2026-02-12 16:56:35 +0530

    Backend API: Fetching Orders for the Logged-In Seller

--- src/main/java/com/quickmart/service/OrderServiceImpl.java
+++ src/main/java/com/quickmart/service/OrderServiceImpl.java
@@ -7,6 +7,7 @@ import com.quickmart.payload.OrderDTO;
 import com.quickmart.payload.OrderItemDTO;
 import com.quickmart.payload.OrderResponse;
 import com.quickmart.repositories.*;
+import com.quickmart.util.AuthUtil;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,6 +48,9 @@ public class OrderServiceImpl implements OrderService{
 
     @Autowired
     ModelMapper modelMapper;
+
+    @Autowired
+    AuthUtil  authUtil;
 
     @Override
     @Transactional
@@ -152,5 +156,40 @@ public class OrderServiceImpl implements OrderService{
         orderRepository.save(order);
 
         return modelMapper.map(order, OrderDTO.class);
+    }
+
+    @Override
+    public OrderResponse getAllSellerOrders(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder) {
+
+        Sort sortByAndOrder = sortOrder.equalsIgnoreCase("asc")
+                ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+        Pageable pageDetails = PageRequest.of(pageNumber, pageSize, sortByAndOrder);
+
+        User seller = authUtil.loggedInUser();
+
+        Page<Order> pageOrders = orderRepository.findAll(pageDetails);
+        List<Order> sellerOrders = pageOrders.getContent().stream()
+                .filter(order -> order.getOrderItems().stream()
+                        .anyMatch(orderItem -> {
+                            var product = orderItem.getProduct();
+                            if(product == null || product.getUser() == null){
+                                return false;
+                            }
+                            return product.getUser().getUserId().equals(seller.getUserId());
+                        })).toList();
+
+        List<OrderDTO> orderDTOs = sellerOrders.stream().map(
+                order ->
+                        modelMapper.map(order, OrderDTO.class)).toList();
+        OrderResponse orderResponse = new OrderResponse();
+        orderResponse.setContent(orderDTOs);
+        orderResponse.setPageNumber(pageOrders.getNumber());
+        orderResponse.setPageSize(pageOrders.getSize());
+        orderResponse.setTotalPages(pageOrders.getTotalPages());
+        orderResponse.setTotalElements(pageOrders.getTotalElements());
+        orderResponse.setLastPage(pageOrders.isLast());
+        return orderResponse;
+
     }
 }
